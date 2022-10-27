2f9a06cc7f701c4335c77fc97a2e8e58bdfce9b0
ferdar7 <ferdar7@gmail.com>
2022-10-27 13:51:06 +0700

    Commit

--- umkm/views.py
+++ umkm/views.py
@@ -4,6 +4,9 @@ from django.http.response import HttpResponse, HttpResponseRedirect, JsonRespons
 from django.urls import reverse
 from django.core import serializers
 from umkm.models import UMKM
+from django.shortcuts import redirect
+
+
 def index(request):
     return render(request, 'umkm_index.html')
 
@@ -12,7 +15,7 @@ def add_umkm(request):
         form = UMKMForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('umkm:show_data'))
+            return HttpResponseRedirect(reverse('umkm:rekomendasi_umkm'))
     else:
         form = UMKMForm()
         response = {'form': form}
@@ -31,3 +34,8 @@ def show_data(req):
     }
 
     return render(req, "umkm_index.html", ctx)
+
+def delete_card(request, pk):
+    UMKM.objects.get(id=pk).delete()
+    return redirect('umkm:rekomendasi_umkm')
+    
