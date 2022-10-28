c48c7d1971064b6eca0c0ff7395f6e75fc1cc4dc
Alanna <alanna@alanna.local>
2022-10-28 13:09:53 +0700

    Fix authentication

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1)
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+
+    def is_tourist(self):
+        if self.status == 'T':
+            return True
+        return False
+
+    def is_local(self):
+        if self.status == 'L':
+            return True
+        return False
+        
+    def __str__(self):
+        return self.user.username
--- project_django/settings.py
+++ project_django/settings.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'main',
     'forum',
     'example_app',
-    'umkm'
+    'umkm',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
