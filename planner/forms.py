2ddf5f0abb1d57668d527b9fd866614190560b5f
Alanna <alanna@ui.ac.id>
2022-11-01 01:13:20 +0700

    Planner done

--- planner/forms.py
+++ planner/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from django.forms import ModelForm, widgets
+from django.forms import ModelForm
 from planner.models import Trips
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, Div
 
 class updateTripForm(ModelForm):
     name = forms.CharField(required=False)
@@ -11,3 +13,16 @@ class updateTripForm(ModelForm):
     class Meta:
         model = Trips
         fields = ['name', 'start_date', 'end_date', 'notes']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row('name'),
+            Row(
+                Column('start_date', css_class='col-md-6 col-lg-6 p-0'),
+                Column('end_date', css_class='col-md-6 col-lg-6 p-0'),
+                css_class='d-flex justify-content-between'
+            ),
+            Row(Column('notes'))
+        )
