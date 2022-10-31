bced8f82ab7e034b2d12a58b2151f26ae4aa0784
Muhammad Iqbal Dwitama <muhammad.iqbal111@ui.ac.id>
2022-10-31 11:38:50 +0700

    fix template

--- forum/urls.py
+++ forum/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from requests import delete
 
-from forum.views import add_question_ajax, show_forum, add_question, add_answer, show_json, delete_task
+from forum.views import *
 
 app_name = "forum"
 
 urlpatterns = [
     path('', show_forum, name='show_forum'),
-    path('add_question/', add_question, name='add_question'),
-    path('add_answer/<int:pk>', add_answer, name='add_answer'),
-    path('json/', show_json, name='show_json'),
-    path('add/', add_question_ajax, name='add_question_ajax'),
-    path('delete/<int:pk>', delete_task, name='delete_task'),
+    path('add_question/', add_question_ajax, name='add_question'),
+    path('add_answer/<int:pk>', add_answer_ajax, name='add_answer'),
+    path('delete_question/<id>', delete_forum_ajax, name='delete_question'),
+    path('question_json/', question_json, name='question_json'),
+    path('answer_json/', answer_json, name='answer_json'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from hashlib import new
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from forum.models import Question, Answer
 from forum.forms import AnswerForm, QuestionForm
+from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 from django.urls import reverse
+import datetime
 
 # Create your views here.
 
@@ -18,39 +21,92 @@ def show_forum(request):
         "list_of_answers" : answer_data,
         "question_form" : question_form,
         "answer_form" : answer_form,
-        "username" : request.user.username
     }
     return render(request, 'forum.html', context)
 
-""" def add_question(request):
-    if request.method == "POST":
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            user = request.user
-            title = request.POST.get('title')
-            question = request.POST.get('question')
+def question_json(request):
+    data = Question.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
 
-            Question.objects.create(
-                user = user,
-                title = title,
-                question = question
-            )
+def answer_json(request):
+    data = Answer.objects.all()
+    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
 
-            return JsonResponse({
-                'error' : False,
-                'msg' : 'Successful'
-            }) """
-
+@csrf_exempt
 def add_question_ajax(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         question = request.POST.get('question')
+
+        new_question = Question.objects.create(
+            user = request.user,
+            username = request.user.username,
+            title = title,
+            question = question,
+            date = datetime.date.today()
+            )
+        
+        result = {
+            'pk': new_question.pk,
+            'fields': {
+                'username': new_question.user.username,
+                'title': new_question.title,
+                'question': new_question.question,
+                'date': new_question.date
+            }
+        }
+        return JsonResponse(result)
+
+@csrf_exempt
+def add_answer_ajax(request, pk):
+    question = Question.objects.get(pk=pk)
+    question.is_answered = True
+    question.save()
+
+    if request.method == "POST":
+        answer = request.POST.get('answer')
+        question = request.POST.get('question')
+
+        answer = Answer.objects.create(
+            user = request.user,
+            username = request.user.username,
+            question = question,
+            answer = answer,
+            date = datetime.date.today()
+            )
+        
+        result = {
+            'pk': answer.pk,
+            'fields': {
+                'question': answer.question,
+                'answer': answer.answer,
+            }
+        }
+        return JsonResponse(result)
+
+
+
+
+        
+    """ form = AnswerForm(request.POST)
+    if form.is_valid():
         user = request.user
+        question = get_question
+        answer = request.POST.get('answer') 
 
-        Question.objects.create(user=user, title=title, question=question)
-        return JsonResponse({'error': False})
+        Answer.objects.create(
+            user = user,
+            question = question,
+            answer = answer
+        )
 
-def add_question(request):
+        return HttpResponseRedirect(reverse('forum:show_forum'))
+else:
+    form = AnswerForm()
+    response = {'form': form}
+    return render(request, 'add_answer.html', response) """
+
+""" def add_question(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST, request.FILES)
         if form.is_valid():
@@ -67,36 +123,13 @@ def add_question(request):
     else:
         form = QuestionForm()
         response = {'form': form}
-        return render(request, 'add_question.html', response)
+        return render(request, 'add_question.html', response) """
         
-def add_answer(request, pk):
-    get_question = Question.objects.get(pk=pk)
-    get_question.is_answered = True
-    get_question.save()
 
-    if request.method == "POST":
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            user = request.user
-            question = get_question
-            answer = request.POST.get('answer') 
 
-            Answer.objects.create(
-                user = user,
-                question = question,
-                answer = answer
-            )
-
-            return HttpResponseRedirect(reverse('forum:show_forum'))
-    else:
-        form = AnswerForm()
-        response = {'form': form}
-        return render(request, 'add_answer.html', response)
-
-def delete_task(request, pk):
-    Question.objects.get(pk=pk).delete()
-    return HttpResponseRedirect(reverse('forum:show_forum'))
-
-def show_json(request):
-    data = Question.objects.all()
-    return HttpResponse(serializers.serialize('json', data), content_type='application/json')
+@csrf_exempt
+def delete_forum_ajax(request, id):
+    if request.method == "DELETE":
+        question = get_object_or_404(Question, id = id)
+        question.delete()
+    return HttpResponse(status=202)
