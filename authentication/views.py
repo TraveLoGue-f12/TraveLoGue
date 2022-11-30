42c878262fcaba7e2de0c9e4e4b38bb693cf5dbe
radityahnf <ferdar7@gmail.com>
2022-11-30 11:30:43 +0700

    added flutter register

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.backends import UserModel
+
 
 @csrf_exempt
 def login(request):
@@ -28,3 +31,27 @@ def login(request):
             "status": False,
             "message": "Failed to Login, check your email/password."
             }, status=401)
+    
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        full_name = data["full_name"]
+        email = data["email"]
+        password1 = data["password1"]
+        status = data["status"]
+        username = data["username"]
+
+        newUser = UserModel.objects.create_user(
+        username = username, 
+        email = email,
+        password = password1,
+        status = status,
+        full_name = full_name
+        )
+
+        newUser.save()
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
