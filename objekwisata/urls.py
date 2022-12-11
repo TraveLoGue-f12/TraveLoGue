bee90421c899aaa5f083e00ebc37defb136913a6
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-12-11 17:07:45 +0700

    update url add flutter

--- objekwisata/urls.py
+++ objekwisata/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from objekwisata.views import index, show_objekwisata_json, add_objekwisata_ajax
+from objekwisata.views import index, show_objekwisata_json, add_objekwisata_ajax, add_flutter
 
 app_name = 'objekwisata'
 
 urlpatterns = [
     path('', index, name='index'),
     path('json/', show_objekwisata_json, name='show_objekwisata_json'),
-    path('add_objekwisata_ajax/', add_objekwisata_ajax, name='add_objekwisata_ajax')
+    path('add_objekwisata_ajax/', add_objekwisata_ajax, name='add_objekwisata_ajax'),
+    path('add_flutter/', add_flutter, name='add_flutter'),
 ]
