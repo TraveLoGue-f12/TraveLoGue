060cfceb310d795622018487cef4ede0162da0d7
ahmad.fatahillah <ahmadfatahillah16@gmail.com>
2022-12-12 11:05:28 +0700

    update model objekwisata

--- objekwisata/models.py
+++ objekwisata/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class ObjekWisata(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=40)
-    description = models.CharField(max_length=100)
-    location = models.CharField(max_length=20)
+    title = models.TextField()
+    description = models.TextField()
+    location = models.TextField()
     address_link = models.URLField()
